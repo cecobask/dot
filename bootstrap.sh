5ff85544a8b82128a998e1cc269a41497848b57a
Tsvetoslav Dimov <baskski@gmail.com>
2024-10-16 16:46:45 +0100

    add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle install
+
+SSH_DIR="$HOME/.ssh"
+SSH_KEY="$SSH_DIR/personal"
+SSH_CONFIG="$SSH_DIR/config"
+ssh-keygen -t ed25519 -C "baskski@gmail.com" -f "$SSH_KEY" -N ''
+eval "$(ssh-agent -s)"
+cat <<EOF >"$SSH_CONFIG"
+Host github.com
+  AddKeysToAgent yes
+  IdentityFile $SSH_KEY
+EOF
+ssh-add --apple-use-keychain "$SSH_KEY"
+pbcopy <"$SSH_KEY.pub"
+echo "Copied ssh public key from location $SSH_KEY to clipboard. Please add it to GitHub..."
+while true; do
+    read -r -p "Done? [y/n] " -n 1
+    echo
+    case "$REPLY" in
+    y | Y) break ;;
+    n | N) exit 1 ;;
+    *) echo "Invalid input" ;;
+    esac
+done
+ssh -T git@github.com
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+git clone --bare git@github.com:cecobask/dot.git "$HOME/.dot"
+alias dot='/usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME'
+dot checkout
+dot config --local status.showUntrackedFiles no
